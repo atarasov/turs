55b042c58f574ff925001a95ba32edd8082e60a3
Alexey <lexa-tarasov@yandex.ru>
2011-12-27 02:05:29 +0400

    dd

--- app/models/journal_photo.rb
+++ app/models/journal_photo.rb
@@ -1,7 +1,7 @@
 class JournalPhoto < ActiveRecord::Base
   belongs_to :journal
 
-  has_attached_file :image, :styles => {:small => "150x150#", :large => "700x700>", :thumbnail => "60x60#", :middle => "360x345>"},
+  has_attached_file :image, :styles => {:small => "250x250>", :large => "700x700>", :thumbnail => "60x60#", :middle => "360x345>"},
                     :url  => "/system/assets/facility_photos/images/:id_partition/:style_:id.:extension",
                     :path => ":rails_root/public/system/assets/facility_photos/images/:id_partition/:style_:id.:extension"
 
