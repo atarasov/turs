602b302c2ef4a96e7a335c4a833608b0cd7b438f
«alan2006» <anoam@yandex.ru>
2012-01-31 00:05:01 +0400

    anonim's comments closed

--- app/controllers/company/comments_controller.rb
+++ app/controllers/company/comments_controller.rb
@@ -1,5 +1,7 @@
 class Company::CommentsController < Company::BaseController
   uses_tiny_mce(:options => AppConfig.for_comment_mce_options, :only => [:index])
+  before_filter :login_required, :only => :add_comment
+
   def index
     @user = User.find(params[:id])
     @comments = @user.comments.recent.all
